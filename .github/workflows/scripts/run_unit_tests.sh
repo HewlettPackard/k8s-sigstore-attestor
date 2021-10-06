9b0c2c4b1a5193d8d62ca2a1145419dd9506be7a
Willian Alves <wra@cesar.org.br>
2021-10-06 10:03:11 -0400

    Added dependencies for tests

--- .github/workflows/scripts/run_unit_tests.sh
+++ .github/workflows/scripts/run_unit_tests.sh
@@ -6,6 +6,8 @@ COVERPROFILE=
 if [ -n "${COVERALLS_TOKEN}" ]; then
     COVERPROFILE=profile.cov
     go get github.com/mattn/goveralls@v0.0.7
+    go get github.com/google/go-containerregistry/pkg/name
+    go get github.com/sigstore/cosign/pkg/cosign
 fi
 
 COVERPROFILE="${COVERPROFILE}" make test
